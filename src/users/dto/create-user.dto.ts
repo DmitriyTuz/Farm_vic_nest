b038ac162f3069d8efc033429a92421aa1a89a63
Dmitriy <dmitriytuz123@gmail.com>
2023-04-24 15:46:07 +0300

    first swagger doc for model User is ready

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateUserDto {
+
+    @ApiProperty({example: 'Alex', description: "Name"})
     readonly name: string;
+    @ApiProperty({example: '12345678', description: "Password"})
     readonly password: string;
+    @ApiProperty({example: '+100000000001', description: "Phone"})
     readonly phone: string;
+    @ApiProperty({example: 'WORKER', description: "Role"})
     readonly type: string;
 }
